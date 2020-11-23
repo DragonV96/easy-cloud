04bd0157bb15c39bfd4f3ae4c3fe62aef69cc4aa
DragonV96 <510537547@qq.com>
2020-11-24 00:23:31 +0800

    [optimize]通用对象优化，添加Spring cloud、swagger依赖

--- common/src/main/java/com/github/cloud/dto/request/PageRequest.java
+++ common/src/main/java/com/github/cloud/dto/request/PageRequest.java
@@ -1,6 +1,10 @@
 package com.github.cloud.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * @author : glw
@@ -8,11 +12,23 @@ import lombok.Data;
  * @time : 12:35
  * @Description : 通用分页实体请求对象
  */
+@EqualsAndHashCode
+@ToString
 @Data
+@ApiModel(value = "通用分页实体请求对象", description = "通用分页实体请求对象")
 public class PageRequest {
 
-    private final long current;
+    private static final long serialVersionUID = 1L;
 
-    private final long size;
+    @ApiModelProperty(value = "当前页码", name = "current", required = true)
+    private long current = 1;
 
+    @ApiModelProperty(value = "当前页大小", name = "size", required = true)
+    private long size = 10;
+
+    @ApiModelProperty(value = "排序字段", name = "column")
+    private String column;
+
+    @ApiModelProperty(value = "排序顺序（ASC 或 DESC）", name = "order")
+    private String order;
 }
