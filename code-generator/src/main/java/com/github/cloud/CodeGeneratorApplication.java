5c210999b4ba0403db3d0fb3179b56979a46cb81
DragonV96 <510537547@qq.com>
2020-12-06 14:54:10 +0800

    [feature]添加代码生成请求、响应、实体、mapper模板

--- code-generator/src/main/java/com/github/cloud/CodeGeneratorApplication.java
+++ code-generator/src/main/java/com/github/cloud/CodeGeneratorApplication.java
@@ -1,10 +1,18 @@
 package com.github.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author : glw
  * @date : 2020/11/28
  * @time : 23:58
  * @Description : 代码生成器
  */
+@SpringBootApplication
 public class CodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGeneratorApplication.class, args);
+    }
 }
