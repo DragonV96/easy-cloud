d812624275ae586bdeaa0f65a4eb8aedfe50c232
DragonV96 <510537547@qq.com>
2020-12-20 15:24:16 +0800

    [feature]完成生成代码后自动停止

--- code-generator/src/main/java/com/github/cloud/handler/GeneratorHandler.java
+++ code-generator/src/main/java/com/github/cloud/handler/GeneratorHandler.java
@@ -1,27 +1,19 @@
 package com.github.cloud.handler;
 
-import cn.hutool.core.io.FileUtil;
 import com.github.cloud.config.*;
-import com.github.cloud.constant.Constant;
-import com.github.cloud.util.VelocityUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
+import com.github.cloud.service.GeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.io.StringWriter;
-import java.util.List;
 
 /**
  * @author : glw
- * @datetime : 2020/12/16 22:48
- * @description : 代码生成器处理器
+ * @datetime : 2020/12/18 00:28
+ * @description : 代码生成处理器
  */
-@Slf4j
 @Component
-public class GeneratorHandler {
+public class GeneratorHandler implements CommandLineRunner {
 
     @Autowired
     private PackageConfig packageConfig;
@@ -38,17 +30,16 @@ public class GeneratorHandler {
     @Autowired
     private TableConfig tableConfig;
 
-    public void outputCode(ProjectConfig projectConfig, PackageConfig packageConfig, SwitchConfig switchConfig, SuffixConfig suffixConfig, TableConfig tableConfig){
-        VelocityContext velocityContext = VelocityUtil.buildContext(projectConfig, packageConfig, switchConfig, suffixConfig, tableConfig);
-        List<String> templateList = VelocityUtil.getTemplateList();
+    @Autowired
+    private GeneratorService generatorService;
 
-        for (String template : templateList) {
-            // 生成模板
-            StringWriter sw = new StringWriter();
-            Template resultTemplate = Velocity.getTemplate(template, Constant.UTF8);
-            resultTemplate.merge(velocityContext, sw);
+    @Autowired
+    private ConfigurableApplicationContext context;
 
-            FileUtil.writeUtf8String(sw.toString(), VelocityUtil.getFileName(projectConfig, packageConfig, suffixConfig, tableConfig, template));
-        }
+    @Override
+    public void run(String... args) throws Exception {
+        generatorService.outputCode(projectConfig, packageConfig, switchConfig, suffixConfig, tableConfig);
+        // 关闭程序
+        context.close();
     }
 }
