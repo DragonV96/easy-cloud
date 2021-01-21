ea147ea1e54b5c852a25875946f7182248d7c04e
DragonV96 <510537547@qq.com>
2021-01-21 23:35:30 +0800

    [update]Oauth客户端调用改为负载均衡方式

--- oauth2/oauth2-client/src/main/java/com/github/cloud/config/Oauth2ClientConfig.java
+++ oauth2/oauth2-client/src/main/java/com/github/cloud/config/Oauth2ClientConfig.java
@@ -1,11 +1,11 @@
 package com.github.cloud.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 /**
  * @author : glw
@@ -18,12 +18,12 @@ public class Oauth2ClientConfig {
 
     /**
      * 定义 OAuth2RestTemplate
-     * @param details
-     * @param context
+     * @param factory
      * @return
      */
+    @LoadBalanced
     @Bean
-    public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ProtectedResourceDetails details, OAuth2ClientContext context) {
-        return new OAuth2RestTemplate(details, context);
+    public OAuth2RestTemplate oAuth2RestTemplate(UserInfoRestTemplateFactory factory) {
+        return factory.getUserInfoRestTemplate();
     }
 }
